b50579e8949e3ef9ed61f193902cf0da1be12590
Jon Zumbrun <jzumbrun@gmail.com>
2016-08-02 23:06:17 -0500

    remove unorm and latinize

--- s3upload.js
+++ s3upload.js
@@ -3,9 +3,6 @@
  * https://github.com/flyingsparx/NodeDirectUploader
  */
 
-var latinize = require('latinize'),
-    unorm = require('unorm');
-
 S3Upload.prototype.server = '';
 S3Upload.prototype.signingUrl = '/sign-s3';
 S3Upload.prototype.fileElement = null;
@@ -63,8 +60,7 @@ S3Upload.prototype.createCORSRequest = function(method, url) {
 };
 
 S3Upload.prototype.executeOnSignedUrl = function(file, callback) {
-    var normalizedFileName = unorm.nfc(file.name.replace(/[!\^`><{}\[\]()*#%'"~|&@:;$=+?\s\\\/\x00-\x1F\x7f]+/ig, '_'));
-    var fileName = latinize(normalizedFileName);
+    var fileName = file.name.replace(/[!\^`><{}\[\]()*#%'"~|&@:;$=+?\s\\\/\x00-\x1F\x7f]+/ig, '_');
     var queryString = '?objectName=' + fileName + '&contentType=' + encodeURIComponent(file.type);
     if (this.signingUrlQueryParams) {
         var signingUrlQueryParams = this.signingUrlQueryParams;
@@ -134,8 +130,8 @@ S3Upload.prototype.uploadToS3 = function(file, signResult) {
                 disposition = 'attachment';
             }
         }
-        var normalizedFileName = unorm.nfc(file.name.replace(/[!\^`><{}\[\]()*#%'"~|&@:;$=+?\s\\\/\x00-\x1F\x7f]+/ig, '_'));
-        var fileName = latinize(normalizedFileName);
+
+        var fileName = file.name.replace(/[!\^`><{}\[\]()*#%'"~|&@:;$=+?\s\\\/\x00-\x1F\x7f]+/ig, '_')
         xhr.setRequestHeader('Content-Disposition', disposition + '; filename=' + fileName);
     }
     if (this.uploadRequestHeaders) {
